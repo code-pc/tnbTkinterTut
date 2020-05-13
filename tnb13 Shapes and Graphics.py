bd6961684552cb78d59f550c68f81d2515a3ee87
Chandan Pandey <indiaatwork@outlook.com>
2020-05-13 12:27:39 +0530

    edited an element
    new yellowLine

--- tnb13 Shapes and Graphics.py
+++ tnb13 Shapes and Graphics.py
@@ -8,7 +8,7 @@ canvas.pack()
 blackLine = canvas.create_line(0,0,200,50)
 blueLine = canvas.create_line(0,100,200,50,fill="red")
 greenBox = canvas.create_rectangle(40,40,100,250,fill="green")
-
+yellowLine = canvas.create_line(30,20,200,50)
 canvas.delete(blueLine)
 
 root.mainloop()
